6e96f02d4b85a8ae671b4a837a2220934c29dda4
a.marmo <a.marmo@studenti.unibg.it>
2023-03-15 10:16:45 +0100

    Prova commit

--- Gruppo_20/src/Progetto/Test.java
+++ Gruppo_20/src/Progetto/Test.java
@@ -5,8 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("test first commit");
-
-
+		System.out.println("test second commit");
+		
 	}
 
 }
