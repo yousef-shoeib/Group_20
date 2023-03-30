89876cbc12770bfb9e2892c5add8f38ad66a04f1
DarioRota <rota.dario.studente@itispaleocapa.it>
2023-03-30 12:23:54 +0200

    Implementate getter e setter per ogni attributo e il costruttore.

--- Gruppo_20/src/progetto/ItemTile.java
+++ Gruppo_20/src/progetto/ItemTile.java
@@ -3,4 +3,34 @@ package progetto;
 public class ItemTile {
 
 	private String Color;
+	private int x;
+	private int y;
+	
+	public String getColor() {
+		return Color;
+	}
+	public void setColor(String color) {
+		Color = color;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public ItemTile (String Color, int x, int y)
+	{
+		this.Color=Color;
+		this.x=x;
+		this.y=y;
+		
+		
+	}
 }
