9c4b9a2fb9b71b5eabf35475ceb004bc1e4f3d12
Marco <Marco@LAPTOP-KSFJPCHT>
2023-03-30 12:19:15 +0200

    Create class Slot

--- Gruppo_20/src/progetto/Slot.java
+++ Gruppo_20/src/progetto/Slot.java
@@ -1,5 +1,32 @@
 package progetto;
 
 public class Slot {
+	private boolean state;
+	private ItemTile itemTile;
+	
+	public ItemTile getItemTile() {
+		return itemTile;
+	}
+
+	public void setItemTile(ItemTile itemTile) {
+		this.itemTile = itemTile;
+	}
+
+	public String isState() {
+		if(state)
+			return "T";
+
+		return "F";
+	}
+
+	public void setStato(boolean state) {
+		this.state = stato;
+	}
+
+	public Slot()
+	{
+		this.state = false;
+		this.itemTile = null;
+	}
 
 }
