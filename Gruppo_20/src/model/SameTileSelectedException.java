addee30d9a5d0989df69e5c22b8990d7d180f77e
Marco <m.rigaglia@studenti.unibg.it>
2023-05-07 09:30:17 +0200

    Add SameTileSelectectedException.

---
+++ Gruppo_20/src/model/SameTileSelectedException.java
@@ -0,0 +1,10 @@
+package model;
+
+@SuppressWarnings("serial")
+public class SameTileSelectedException extends Exception {
+
+	public SameTileSelectedException (String message)
+	{
+		super(message);
+	}
+}
