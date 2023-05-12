a7582d81e6c74bb1ff4c682a4f94c539ce1bfe81
a.marmo <a.marmo@studenti.unibg.it>
2023-05-12 09:07:08 +0200

    Aggiunta classe CommonGoalCard6

---
+++ Gruppo_20/src/commongoal/CommonGoalCard6.java
@@ -0,0 +1,42 @@
+package commongoal;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import model.Bookshelf;
+import model.ItemTile;
+
+public class CommonGoalCard6 extends CommonGoalCard {
+	Bookshelf bookshelf;
+	public CommonGoalCard6(Bookshelf bookshelf) {
+		this.bookshelf = bookshelf;
+	}
+	@Override
+	boolean CheckTarget(Bookshelf b) {
+		int unique_rows=0;
+		for(int row1=0; row1 < bookshelf.getRows(); row1++) {
+			for(int row2=row1+1; row2<bookshelf.getRows(); row2++) {
+				boolean only_uniques = true;
+				Set<ItemTile> TilesRow1 = new HashSet<ItemTile>();
+				Set<ItemTile> TilesRow2 = new HashSet<ItemTile>();
+				for(int column=0; column<bookshelf.getColumns(); column++) {
+					ItemTile tiles1 = bookshelf.getTile(row1, column);
+					ItemTile tiles2 = bookshelf.getTile(row2, column);
+					if(TilesRow1.contains(tiles1) || TilesRow2.contains(tiles2)) {
+						only_uniques = false;
+						break;
+					}
+					TilesRow1.add(tiles1);
+	                TilesRow2.add(tiles2);
+				}
+				if (only_uniques) {
+	                unique_rows++;
+	                if (unique_rows == 1) {
+	                    return true;
+	                }
+			    }
+		    }
+	    }
+	 return false;
+  }
+}
