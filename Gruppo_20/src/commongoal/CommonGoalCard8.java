46ff680860cf70aaf711a97c4a2209a4b48c764f
a.marmo <a.marmo@studenti.unibg.it>
2023-05-12 18:45:32 +0200

    Rinominazione classi CommonGoal

---
+++ Gruppo_20/src/commongoal/CommonGoalCard8.java
@@ -0,0 +1,24 @@
+package commongoal;
+import model.Bookshelf;
+public class CommonGoalCard8 extends CommonGoalCard{
+	Bookshelf bookshelf;
+	public CommonGoalCard8(Bookshelf bookshelf) {
+		this.bookshelf = bookshelf;
+	}
+	@Override
+	boolean CheckTarget() {
+		boolean check = false;
+		if(this.bookshelf.getSlot(0, 0).isEmpty()==false && 
+		   this.bookshelf.getSlot(0, 4).isEmpty()==false &&
+		   this.bookshelf.getSlot(5, 4).isEmpty()==false &&
+		   this.bookshelf.getSlot(5, 0).isEmpty()==false) {
+					if(this.bookshelf.getTile(0, 0)==this.bookshelf.getTile(0, 4) &&
+				       this.bookshelf.getTile(0, 4)==this.bookshelf.getTile(5, 4) &&
+					   this.bookshelf.getTile(5, 4)==this.bookshelf.getTile(5, 0)) {
+			                             check = true;
+					}
+		}
+		return check;
+	}
+	
+}
