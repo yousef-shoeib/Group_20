2121d1e767391ca7e03a3f1a8d15e0d11a846453
a.marmo <a.marmo@studenti.unibg.it>
2023-05-12 16:51:10 +0200

    Aggiunta classe CommonGoalCard04

---
+++ Gruppo_20/src/commongoal/CommonGoalCard04.java
@@ -0,0 +1,49 @@
+package commongoal;
+import model.Slot;
+import model.Bookshelf;
+
+public class CommonGoalCard04 extends CommonGoalCard{
+	Bookshelf bookshelf;
+	public CommonGoalCard04(Bookshelf bookshelf) {
+		this.bookshelf = bookshelf;
+	}
+	
+	private int countHorizontal() {
+		int count=0;
+		for(int i=0; i<this.bookshelf.getRows(); i++) {
+			for(int j=0; j<this.bookshelf.getColumns()-1; j++) {
+				if(this.bookshelf.getSlot(i, j).isEmpty() == false &&
+				   this.bookshelf.getSlot(i, j+1).isEmpty() == false) {
+							if(this.bookshelf.getTile(i, j) == this.bookshelf.getTile(i, j+1)) count++;
+				}
+			}
+		}
+		return count;
+	}
+	
+	private int countVertical() {
+		int count=0;
+		for(int j=0; j<bookshelf.getColumns(); j++) {
+			for(int i=0; i<bookshelf.getRows()-1; i++) {
+				if(this.bookshelf.getSlot(i, j).isEmpty() == false &&
+				   this.bookshelf.getSlot(i+1,j).isEmpty()==false) {
+							if(this.bookshelf.getTile(i, j) == this.bookshelf.getTile(i+1, j)) count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	@Override
+	boolean CheckTarget() {
+		int countVertical, countHorizontal;
+		countVertical = countVertical();
+		countHorizontal = countHorizontal();
+		if((countHorizontal+countVertical)>=6) return true;
+		else return false;
+	}
+	
+	
+	
+	
+}
