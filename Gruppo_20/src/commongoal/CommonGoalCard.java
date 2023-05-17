10c8bbd862cecb4701461c7c934b45e26386944e
a.marmo <a.marmo@studenti.unibg.it>
2023-05-17 08:32:39 +0200

    Aggiornamento classi Common

--- Gruppo_20/src/commongoal/CommonGoalCard.java
+++ Gruppo_20/src/commongoal/CommonGoalCard.java
@@ -14,51 +14,51 @@ public abstract class CommonGoalCard {
 	abstract boolean CheckTarget();
 	
 	
-	/*
+	
 	public static CommonGoalCard assignCommonGoalCard(Bookshelf b) {
 		Random random = new Random();
 		int num = random.nextInt(12)+1;
 		CommonGoalCard card = null;
 		switch(num) {
 			case 1:
-				card = new CommonGoalCard1();
+				card = new CommonGoalCard1(b);
 				break;
 			case 2:
-				card = new CommonGoalCard2();
+				card = new CommonGoalCard2(b);
 				break;
 			case 3:
-				card = new CommonGoalCard3();
+				card = new CommonGoalCard3(b);
 				break;
 			case 4:
-				card = new CommonGoalCard4();
+				card = new CommonGoalCard4(b);
 				break;
-			case 5:
-				card = new CommonGoalCard5();
-				break;
+			//case 5:
+				//card = new CommonGoalCard5();
+				//break;
 			case 6:
-				card = new CommonGoalCard6();
+				card = new CommonGoalCard6(b);
 				break;
-			case 7:
-				card = new CommonGoalCard7();
-				break;
+			//case 7:
+				//card = new CommonGoalCard7(b);
+				//break;
 			case 8:
-				card = new CommonGoalCard8();
+				card = new CommonGoalCard8(b);
 				break;
 			case 9:
-				card = new CommonGoalCard9();
+				card = new CommonGoalCard9(b);
 				break;
 			case 10:
-				card = new CommonGoalCard10();
+				card = new CommonGoalCard10(b);
 				break;
 			case 11:
-				card = new CommonGoalCard11();
+				card = new CommonGoalCard11(b);
 				break;
-			case 12:
-				card = new CommonGoalCard12();
-				break;
+			//case 12:
+				//card = new CommonGoalCard12();
+				//break;
 			}
 		return card;
 	}
-	*/
+	
 	
 }
