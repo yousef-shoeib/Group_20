90da07deca71a9355dac380c82637311435b5501
a.marmo <a.marmo@studenti.unibg.it>
2023-05-17 10:01:22 +0200

    Aggiunta classe CommonGoalCard5

---
+++ Gruppo_20/src/commongoal/CommonGoalCard5.java
@@ -0,0 +1,42 @@
+package commongoal;
+
+import model.Bookshelf;
+import model.ItemTile;
+import model.ItemTileType;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class CommonGoalCard5 extends CommonGoalCard{
+	Bookshelf bookshelf;
+	public CommonGoalCard5(Bookshelf bookshelf) {
+		this.bookshelf = bookshelf;
+	}
+	
+	private boolean searchColumns() {
+		int countColumns=0;
+		for(int j=0; j<this.bookshelf.getColumns(); j++) {
+			Set<ItemTileType> differentTypes = new HashSet<>();
+			for(int i=0; i<this.bookshelf.getRows(); i++) {
+				if(this.bookshelf.getSlot(i, j).isEmpty() == false) {
+					differentTypes.add(this.bookshelf.getItemTileType(i,j));
+					if(differentTypes.size()>3) {
+						break;
+					}
+				}
+			}
+			if(differentTypes.size()<=3) countColumns++;
+		}
+		if(countColumns>=3) return true;
+		else return false;
+	}
+	
+	@Override
+	boolean CheckTarget() {
+		boolean check;
+		check = searchColumns();
+		if(check) return true;
+		else return false;
+	}
+	
+}
