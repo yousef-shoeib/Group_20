7866eb1ec15c774a71c3f68f21c580391e9bb4aa
marcUniv <m.rigaglia@studenti.unibg.it>
2023-05-27 16:50:35 +0200

    Optimized takeableTile. Added JavaDoc to LivingRoomBoard

--- Gruppo_20/src/model/LivingRoomBoard.java
+++ Gruppo_20/src/model/LivingRoomBoard.java
@@ -3,6 +3,7 @@ package model;
 import java.util.List;
 import java.util.Random;
 
+import exception.EqualsTilesException;
 import exception.TileAlreadySelectedException;
 import exception.TileHasNotFreeSideException;
 import exception.TilesAreNotAdjacentException;
@@ -16,7 +17,7 @@ import exception.TilesAreNotInlineException;
  */
 public class LivingRoomBoard extends Grid {
 
-	private int[][] configMatrTwoPlayer = {	{0,0,0,0,0,0,0,0,0}, 
+	private final int[][] configMatrTwoPlayer = {	{0,0,0,0,0,0,0,0,0}, 
 											{0,0,0,1,1,0,0,0,0},
 											{0,0,0,1,1,1,0,0,0},
 											{0,0,1,1,1,1,1,1,0},
@@ -26,7 +27,7 @@ public class LivingRoomBoard extends Grid {
 											{0,0,0,0,1,1,0,0,0},
 											{0,0,0,0,0,0,0,0,0}};
 	
-	private int[][] configMatrThreePlayer = {	{0,0,0,1,0,0,0,0,0}, 
+	private final int[][] configMatrThreePlayer = {	{0,0,0,1,0,0,0,0,0}, 
 												{0,0,0,1,1,0,0,0,0},
 												{0,0,1,1,1,1,1,0,0},
 												{0,0,1,1,1,1,1,1,1},
@@ -36,7 +37,7 @@ public class LivingRoomBoard extends Grid {
 												{0,0,0,0,1,1,0,0,0},
 												{0,0,0,0,0,1,0,0,0}};
 	
-	private int[][] configMatrFourPlayer = {	{0,0,0,1,1,0,0,0,0}, 
+	private final int[][] configMatrFourPlayer = {	{0,0,0,1,1,0,0,0,0}, 
 												{0,0,0,1,1,1,0,0,0},
 												{0,0,1,1,1,1,1,0,0},
 												{0,1,1,1,1,1,1,1,1},
@@ -101,154 +102,70 @@ public class LivingRoomBoard extends Grid {
 			}
 		}
 	}
-	
-	private boolean tileHasFreeSide(ItemTile currentSelectedTile)
-	{
-		Slot currentSlot = this.getSlotFromTile(currentSelectedTile);
-		int x = currentSlot.getX();
-		int y = currentSlot.getY();
-		
-		if(x == 0 || y == 0 || x == 8 || y == 8)
-			return true;
-		
-		Slot slot1 = matrGrid[(x-1)][y];
-		Slot slot2 = matrGrid[x][(y-1)];
-		Slot slot3 = matrGrid[(x+1)][y];
-		Slot slot4 = matrGrid[x][(y+1)];
-		
-		if(!slot1.State() || slot1.isEmpty())
-			return true;
-		if(!slot2.State() || slot2.isEmpty())
-			return true;
-		if(!slot3.State() || slot3.isEmpty())
-			return true;
-		if(!slot4.State() || slot4.isEmpty())
-			return true;
-		
-		return false;
-	}
 	/**
-	 * verifica se due tessere sono adiacenti
-	 * @param currentSelectedTile
-	 * @param lastSelectedTile
-	 * @return vero se le tessere sono adiacenti
+	 * Verifica se una tessera è prendibile.
+	 * @param tileToCheck
+	 * @return vero se è prendibile.
+	 * @throws @TileHasNotFreeSideException se la tessera non ha lati liberi
 	 */
-	public boolean tilesAreAdjacent(ItemTile currentSelectedTile, ItemTile lastSelectedTile)
+	public boolean takeableTile(ItemTile tileToCheck) throws Exception
 	{
-		Slot currentSelectedSlot = this.getSlotFromTile(currentSelectedTile);
-		Slot lastSelectedSlot = this.getSlotFromTile(lastSelectedTile);
-		
-		if(currentSelectedSlot.getX() == lastSelectedSlot.getX())
-		{	
-			if(currentSelectedSlot.getY() - 1 == lastSelectedSlot.getY() || currentSelectedSlot.getY() + 1 == lastSelectedSlot.getY())
-				return true;
+		if(tileToCheck == null){
+			throw new NullPointerException("tile is null");
 		}
-		if(currentSelectedSlot.getY() == lastSelectedSlot.getY())
-		{	
-			if(currentSelectedSlot.getX() - 1 == lastSelectedSlot.getX() || currentSelectedSlot.getX() + 1 == lastSelectedSlot.getX())
-				return true;
-		}
-		
-		return false;
-	}
-
-	private boolean tileIsInline(ItemTile currentSelectedTile, ItemTile lastSelectedTile, ItemTile firstSelectedTile)
-	{
-		Slot currentSelectedSlot = this.getSlotFromTile(currentSelectedTile);
-		Slot lastSelectedSlot = this.getSlotFromTile(lastSelectedTile);
-		Slot firstSelectedSlot = this.getSlotFromTile(firstSelectedTile);
-		
-		if(currentSelectedSlot.getX() == lastSelectedSlot.getX() && currentSelectedSlot.getX() == firstSelectedSlot.getX())
-			return true;
-		if(currentSelectedSlot.getY() == lastSelectedSlot.getY() && currentSelectedSlot.getY() == firstSelectedSlot.getY())
-			return true;
-
-		return false;
-	}
-
-	private boolean tilesAlreadySelected(ItemTile currentSelectedTile, ItemTile lastSelectedTile, ItemTile firstSelectedTile)
-	{
-		if(lastSelectedTile != null)
-		{
-			if(currentSelectedTile.getId() == lastSelectedTile.getId())
-			{
-				return true;
-			}
-		}
-		if(firstSelectedTile != null)
-		{
-			if(currentSelectedTile.getId() == firstSelectedTile.getId())
-			{
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
-	public ItemTile getTile(ItemTile currentSelectedTile, ItemTile lastSelectedTile,ItemTile firstSelectedTile) throws Exception
-	{
-		if(currentSelectedTile == null){
-			throw new NullPointerException("no tile selected");
-		}
-		
-		if(tilesAlreadySelected(currentSelectedTile,lastSelectedTile,firstSelectedTile)){
-			throw new TileAlreadySelectedException("Tile already Getted");
-		}
-		if (!tileHasFreeSide(currentSelectedTile)){
+		if (!tileHasFreeSide(tileToCheck)){
 			throw new TileHasNotFreeSideException("tile has not free side");
-		}
-		
-		if(lastSelectedTile == null){
-			return currentSelectedTile;
-		}
-		
-		if(!tilesAreAdjacent(currentSelectedTile,lastSelectedTile)){
-			if(firstSelectedTile == null)
-				throw new TilesAreNotAdjacentException("tile are not adjacent");
-			else
-				if(!tilesAreAdjacent(currentSelectedTile,firstSelectedTile)){
-					throw new TilesAreNotAdjacentException("tile are not adjacent");
-				}
-		}
-		
-		if(firstSelectedTile == null){
-			return currentSelectedTile;
-		}
-		
-		if(!tileIsInline(currentSelectedTile,lastSelectedTile,firstSelectedTile)){
-			throw new TilesAreNotInlineException("tile are not inline");
-		}
-		
-		return currentSelectedTile;
+		}	
+		return true;
 	}
 	/**
-	 * Rimuove le tessere dalla Plancia Soggiorno
-	 * @param selectedTiles
+	 * verifica se due tessere sono prendibili.
+	 * @param tile1
+	 * @param tile2
+	 * @return vero se le tessere sono prendibili
+	 * @throws @TilesAreNotAdjacentException se le tessere non sono adiacenti
 	 */
-	public void removeTile(List<ItemTile> selectedTiles) {
-		for(ItemTile item : selectedTiles)
-		{
-			this.removeTile(item);
-		}
-	}
-	private int removeTile(ItemTile item) {
+	public boolean takeableTile(ItemTile tile1, ItemTile tile2) throws Exception
+	{
+		boolean result = false;
 		
-		for(int x = 0; x < this.rows; x++)
-		{	
-			for(int y = 0; y < this.columns ; y++)
-			{
-				if(matrGrid[x][y].State() && !matrGrid[x][y].isEmpty())
-				{
-					if(matrGrid[x][y].getItemTile().getId() == item.getId())
-					{
-						matrGrid[x][y].setItemTile(null);;
-						return 1;
-					}
-					
-				}
-			}
+		result = this.takeableTile(tile2);
+		result = this.takeableTile(tile1);
+		
+		if(tile1.equals(tile2)){
+			throw new EqualsTilesException("Tiles are equals");
 		}
-		return -1;
+		if(!tilesAreAdjacent(tile1,tile2)){
+			throw new TilesAreNotAdjacentException("tiles are not adjacent");
+		}
+		return result;
+	}
+	/**
+	 * verifica se tre tessere sono prendibili.
+	 * @param tile1
+	 * @param tile2
+	 * @param tile3
+	 * @return vero se sono prendibili
+	 * @throws @TilesAreNotAdjacentException se non sono adiacenti
+	 * @throws @TilesAreNotInlineException se non sono tutte sulla stessa riga o colonna
+	 */
+	public boolean takeableTile(ItemTile tile1, ItemTile tile2,ItemTile tile3) throws Exception
+	{
+		boolean result = false;
+		
+		result = this.takeableTile(tile1);
+		result = this.takeableTile(tile2);
+		result = this.takeableTile(tile3);
+		
+		if(tile1.equals(tile3) || tile1.equals(tile2) || tile3.equals(tile2)){
+			throw new EqualsTilesException("Almost two Tiles are equals");
+		}
+		if(!tilesAreAdjacent(tile1,tile2,tile3)) {
+				throw new TilesAreNotAdjacentException("tiles are not adjacent");
+		}
+		if(!tileIsInline(tile1,tile2,tile3)){
+			throw new TilesAreNotInlineException("tiles are not inline");
+		}
+		return result;
 	}
 }
