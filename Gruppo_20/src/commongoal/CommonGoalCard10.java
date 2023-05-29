4c2be7c490f4b33fccaeae108143ed50cd901659
a.marmo <a.marmo@studenti.unibg.it>
2023-05-29 11:54:59 +0200

    Test della CommonGoalCard10

--- Gruppo_20/src/commongoal/CommonGoalCard10.java
+++ Gruppo_20/src/commongoal/CommonGoalCard10.java
@@ -15,19 +15,20 @@ public class CommonGoalCard10 extends CommonGoalCard{
 	public boolean CheckTarget(Bookshelf bookshelf) {
 		for(int i=1; i<bookshelf.getRows()-1; i++) {
 			for(int j=1; j<bookshelf.getColumns()-1; j++) {
-				if(!bookshelf.getSlot(i, j).isEmpty()  &&
-				   !bookshelf.getSlot(i-1, j-1).isEmpty()  &&
-				   !bookshelf.getSlot(i+1, j-1).isEmpty()  &&
-				   !bookshelf.getSlot(i-1, j+1).isEmpty()  &&
-				   !bookshelf.getSlot(i+1, j+1).isEmpty() ){
-					ItemTileType type=bookshelf.getTile(i,j).getType();
-					if(bookshelf.getTile(i-1, j-1).getType().equals(type) &&
-					   bookshelf.getTile(i+1, j-1).getType().equals(type) &&
-					   bookshelf.getTile(i-1, j+1).getType().equals(type) &&
-					   bookshelf.getTile(i+1, j+1).getType().equals(type)) {
-						return true;
+				if(!bookshelf.getSlot(i, j).isEmpty()) {
+					if(!bookshelf.getSlot(i-1, j-1).isEmpty()  &&
+				       !bookshelf.getSlot(i+1, j-1).isEmpty()  &&
+				       !bookshelf.getSlot(i-1, j+1).isEmpty()  &&
+				       !bookshelf.getSlot(i+1, j+1).isEmpty()){
+						ItemTileType type = bookshelf.getTile(i, j).getType();
+							if(bookshelf.getTile(i-1, j-1).getType().equals(type) &&
+							   bookshelf.getTile(i+1, j-1).getType().equals(type) &&
+					           bookshelf.getTile(i-1, j+1).getType().equals(type) &&
+					           bookshelf.getTile(i+1, j+1).getType().equals(type)) {
+								return true;
+							}
 					}
-				 }
+				}
 			  }
 		   }
 		return false;
