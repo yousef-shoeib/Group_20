67b700cad7267fe707f7b0caee83b9f367a07289
a.marmo <a.marmo@studenti.unibg.it>
2023-05-29 18:03:58 +0200

    Aggiunta JavaDoc alla CommonGoalCard4

--- Gruppo_20/src/commongoal/CommonGoalCard4.java
+++ Gruppo_20/src/commongoal/CommonGoalCard4.java
@@ -1,7 +1,13 @@
 package commongoal;
 import model.Slot;
 import model.Bookshelf;
-
+/**
+ * La CommonGoalCard4 ha come obiettivo sei gruppi separati formati ciascuno 
+ * da due tessere adiacenti dello stesso tipo, verticalmente o orizzontalmente.
+ * Le tessere di un gruppo possono essere diverse da quelle di un altro gruppo. 
+ * @author Anton
+ *
+ */
 public class CommonGoalCard4 extends CommonGoalCard{
 	
 	private String path = "./resources/Assets/commonGoalCards/4.jpg";
